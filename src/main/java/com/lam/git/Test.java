1f65e8bb554d018e3b754a905b7570e7c3b797e8
JY.Lam <764389274@qq.com>
2022-08-18 16:02:36 +0800

    first commit

---
+++ src/main/java/com/lam/git/Test.java
@@ -0,0 +1,13 @@
+package com.lam.git;
+
+/**
+ * @author: JY.Lam
+ * @version: v 1.0
+ * @create: 2022-08-18 16:02
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
